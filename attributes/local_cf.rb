324ed37e2fcb359f1eae0616a06671f00da914c8
Xabier de Zuazo <xabier@onddo.com>
2013-05-31 18:01:49 +0200

    bayes_ignore_header attribute renamed to bayes_ignore_headers

--- attributes/local_cf.rb
+++ attributes/local_cf.rb
@@ -9,7 +9,7 @@ default['onddo-spamassassin']['conf']['lock_method'] = nil
 default['onddo-spamassassin']['conf']['required_score'] = 5
 default['onddo-spamassassin']['conf']['use_bayes'] = true
 default['onddo-spamassassin']['conf']['bayes_auto_learn'] = true
-default['onddo-spamassassin']['conf']['bayes_ignore_header'] = []
+default['onddo-spamassassin']['conf']['bayes_ignore_headers'] = []
 
 default['onddo-spamassassin']['conf']['plugins']['shortcircuit'] = []
 # default['onddo-spamassassin']['conf']['plugins']['shortcircuit'] = [
