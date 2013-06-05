f7c8c5cef625d2504d502637c7b39e17de62fe8d
Xabier de Zuazo <xabier@onddo.com>
2013-06-05 23:18:12 +0200

    removed a commented attribute

--- attributes/local_cf.rb
+++ attributes/local_cf.rb
@@ -12,21 +12,4 @@ default['onddo-spamassassin']['conf']['bayes_auto_learn'] = true
 default['onddo-spamassassin']['conf']['bayes_ignore_headers'] = []
 
 default['onddo-spamassassin']['conf']['plugins']['shortcircuit'] = []
-# default['onddo-spamassassin']['conf']['plugins']['shortcircuit'] = [
-#   {
-#     'USER_IN_WHITELIST' => 'on',
-#     'USER_IN_DEF_WHITELIST' => 'on',
-#     'USER_IN_ALL_SPAM_TO' => 'on',
-#     'SUBJECT_IN_WHITELIST' => 'on',
-#
-#     'USER_IN_BLACKLIST' => 'on',
-#     'USER_IN_BLACKLIST_TO' => 'on',
-#     'SUBJECT_IN_BLACKLIST' => 'on',
-#
-#     'ALL_TRUSTED' => 'on',
-#
-#     'BAYES_99' => 'spam',
-#     'BAYES_00' => 'ham',
-#   }
-# ]
 
