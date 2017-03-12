e5dac01bbffdb68bd7f1a0603ca3435e0469fc9c
Xabier de Zuazo <xabier@zuazo.org>
2017-03-12 16:05:39 +0100

    Metadata: Add chef_version

--- metadata.rb
+++ metadata.rb
@@ -35,6 +35,8 @@ if respond_to?(:issues_url)
   issues_url 'https://github.com/zuazo/spamassassin-cookbook/issues'
 end
 
+chef_version '>= 12' if respond_to?(:chef_version)
+
 supports 'amazon'
 supports 'centos'
 supports 'debian'
